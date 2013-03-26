88f03c5ced8260598a9e329c35983adfed3ca1c4
Saul Reynolds-Haertle <saulrh@gatech.edu>
2013-03-25 21:30:29 -0400

    xbindjoy is now a guile shell

--- src/xbindjoy.c
+++ src/xbindjoy.c
@@ -1,8 +1,20 @@
 #include <config.h>
 #include <stdio.h>
+
+#include <libguile.h>
+
+/* structure: */
+/*   xbindjoy contains the event loop and as little else as possible */
+/*   joystick contains functions for handling joystick data */
+/*   sender contains functions for sending x events */
+/*   guile contains functions to handle the config script */
+
+
+void inner_main(void* data, int argc, char** argv) {
+    printf("foo!");
+    scm_shell(argc, argv);
+}
           
-int main (void) {
-    puts ("Hello World!");
-    puts ("This is " PACKAGE_STRING ".");
-    return 0;
+int main(int argc, char** argv) {
+    scm_boot_guile(argc, argv, (inner_main), NULL);
 }
